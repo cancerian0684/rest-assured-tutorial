d66e97ae93d07710842eda3ad6692137d88c3672
Munish Chandel <cancerian0684@gmail.com>
2019-11-25 10:57:28 +0530

    Added REST assured spring mvc testcase

---
+++ src/main/java/com/shunya/restassured/DemoController.java
@@ -0,0 +1,23 @@
+package com.shunya.restassured;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+public class DemoController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/greeting", method = GET)
+    @ResponseBody
+    public Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
---
+++ src/main/java/com/shunya/restassured/Greeting.java
@@ -0,0 +1,30 @@
+package com.shunya.restassured;
+
+public class Greeting {
+    long id;
+    String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
